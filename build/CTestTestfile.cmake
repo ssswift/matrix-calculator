90c306f1b86243ceeeac4beab6de9cb487ab28c4
Timofey Strizhov <strizhovtimofey@gmail.com>
2025-09-25 15:16:54 +0300

    Fix CI/CD: clean build cache, simplify configuration

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/swift/matrix-calculator
-# Build directory: /Users/swift/matrix-calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/swift/matrix-calculator/tests
-# Build directory: /Users/swift/matrix-calculator/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[matrix_tests]=] "/Users/swift/matrix-calculator/build/tests/matrix_tests")
-set_tests_properties([=[matrix_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/swift/matrix-calculator/tests/CMakeLists.txt;6;add_test;/Users/swift/matrix-calculator/tests/CMakeLists.txt;0;")
