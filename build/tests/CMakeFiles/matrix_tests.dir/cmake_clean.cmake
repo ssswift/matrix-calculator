463d86e6a838ffe749118b9ae8ffb2f751fa1f90
Timofey Strizhov <strizhovtimofey@gmail.com>
2025-09-25 13:44:03 +0300

    test

---
+++ build/tests/CMakeFiles/matrix_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_tests.dir/test_matrix.cpp.o"
+  "CMakeFiles/matrix_tests.dir/test_matrix.cpp.o.d"
+  "matrix_tests"
+  "matrix_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
